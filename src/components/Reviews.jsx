ab1b9f22d8d135798e891f072d55735de45c366d
theeatch <dhruvnegiclub03@gmail.com>
2023-06-10 10:37:52 +0530

    installed swiper modified navbar responsiveness created reviews for AboutUs Fixed Testimonials Responsiveness Created AboutUs Fixed Products responsiveness

---
+++ src/components/Reviews.jsx
@@ -0,0 +1,123 @@
+import React from "react"
+import Swiper from "swiper"
+import "swiper/swiper-bundle.min.css"
+
+class Reviews extends React.Component {
+  componentDidMount() {
+    new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      grid: {
+        rows: 1,
+      },
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    })
+  }
+
+  render() {
+    return (
+      <div className="container mx-auto mt-20 swiper mySwiper">
+        <div className="mb-16 swiper-wrapper">
+          <div className="swiper-slide w-1/2">
+            <div className="p-4 text-gray-800 rounded-lg shadow-md">
+              <div className="mb-2">
+                <p className="mb-2 text-center text-green-800 text-lg ">
+                  - Mark J. Gage, Richard S. Yoon, Kenneth A. Egol, Frank A.
+                  Liporace, Orthopedic Clinics of North America, Vol 46, 2015.
+                </p>
+                <div className="flex flex-col items-center justify-center">
+                  <p className="text-md text-gray-600 text-center">
+                    “Since its inception more than 20 years ago, negative
+                    pressure wound therapy (NPWT) has had a major impact in the
+                    management of orthopedic injuries. NPWT has been widely
+                    adopted for use in a variety of clinical scenarios, and has
+                    had re- ported success in the setting of high-energy trauma,
+                    open fractures, infections, and excessive soft tissue
+                    damage. “
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="swiper-slide w-1/2">
+            <div className="p-4 text-gray-800 rounded-lg shadow-md">
+              <div className="mb-2">
+                <p className="mb-2 text-center text-green-800 text-lg ">
+                  - David Brinkert Mazen Ali Magali Naud Nicolas Maire Chloé
+                  Trial Luc Téot, International Wound Journal, 2013.
+                </p>
+                <div className="flex flex-col items-center justify-center">
+                  <p className="text-md text-gray-600 text-center">
+                    “Investigators from all three test sites observed a common
+                    positive effect of the saline instillation after a few days
+                    with respect to increased granulation tissue formation and
+                    reduced wound volume. The newly formed granulation tissue
+                    after NPWTi was more beefy red and moist. Granulation tissue
+                    production was enhanced compared to conventional NPWT, in
+                    terms of filling the dead space more rapidly and completely.
+                    Undermined cavities and exposed bones were also more rapidly
+                    covered during NPWTi. The effects of instillation were
+                    likely more striking owing to systematic surgical
+                    debridement prior to initiating NPWTi and at each dressing
+                    change as appropriate.”
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="swiper-slide w-1/2">
+            <div className="p-4 text-gray-800 rounded-lg shadow-md">
+              <div className="mb-2">
+                <p className="mb-2 text-center text-green-800 text-lg ">
+                  - Wound Management & Prevention, Vol 62, Issue 9, 2016 Robert
+                  Dettmers, Wouter Brekelmans, Michiel Leijnen, Boudewijn Borger
+                  van der Burg, Ewan Ritchie.
+                </p>
+                <div className="flex flex-col items-center justify-center">
+                  <p className="text-md text-gray-600 text-center">
+                    “Infection following orthopedic implants for bone fixation
+                    or joint replacement is always serious and may require
+                    removal of the osteosynthetic material. Negative pressure
+                    wound therapy with instillation and dwell time (NPWTi-d) is
+                    an emerging therapy for the treatment of complex wounds,
+                    including infected wounds with osteosynthetic material.”
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="swiper-slide w-1/2">
+            <div className="p-4 text-gray-800 rounded-lg shadow-md">
+              <div className="mb-2">
+                <p className="mb-2 text-center text-green-800 text-lg ">
+                  - Nanda Kishore Maddineni, Satya Kumar Koduru, Harshavardan
+                  Surath, Ananda Rao Venkata Dakshina Murthy, Mettu Rami Reddy,
+                  Amarnath Surath Journal of Dr. NTR University of Health
+                  Sciences, Vol 4, 2015.
+                </p>
+                <div className="flex flex-col items-center justify-center">
+                  <p className="text-md text-gray-600 text-center">
+                    “NPWT has a definite role in eradicating early postoperative
+                    infection in the presence of an implant. The greatest
+                    advantage is retention of the implant. The use of NPWT in
+                    early orthopedic surgical site infection will prevent
+                    implant removal and facilitate rapid formation of
+                    granulation tissue. It will render the wound environment
+                    suitable for soft tissue reconstruction, ultimately leading
+                    to fracture union.”
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="swiper-pagination"></div>
+      </div>
+    )
+  }
+}
+
+export default Reviews
---
+++ src/pages/about.js
@@ -0,0 +1,147 @@
+import React from "react"
+import Layout from "../components/Layout"
+import image from "../images/lifekshell-about-ppic.png"
+import aboutimage from "../images/lifekshell-about-us.png"
+import Reviews from "../components/Reviews"
+
+const about = ({ location }) => {
+  return (
+    <Layout location={location}>
+      <div className="2xl:container 2xl:mx-auto lg:py-16 lg:px-20 md:py-12 md:px-6 py-9 px-4">
+        <p className="font-normal leading-3 cursor-pointer pb-2">WHO WE ARE</p>
+        <div className="flex lg:flex-row flex-col lg:gap-8 sm:gap-10 gap-12">
+          <div className="w-full lg:w-1/2">
+            <h2 className="w-full font-bold lg:text-4xl text-3xl lg:leading-10 leading-9">
+              About LIFE kSHELL
+            </h2>
+            <p className="font-normal text-base  text-gray-600 mt-6">
+              LIFE kSHELL is committed for Excellence by everyway, in everything
+              that we do as professionals with a personal trusted relationship.
+              We believe, each patients deserve the most advanced & innovative
+              treatments available, LIFE kSHELL offers a complete portfolio of
+              advanced, safe and effective Negative Pressure Wound Therapy
+              (NPWT) solutions delivered by experts. Our products LS NPWT
+              Devices (i-WAC & i-WAC Pro) & LS NPWT Dressing Kit have been
+              specially designed to benefits patients from the proven outcomes
+              of NPWT. We provide high quality products and skilled NPWT
+              dressing service facility, supporting patient counseling to full
+              wound closure with dedicated service delivery. LIFE kSHELL is for
+              Cost effective yet excellence in QUALITY, SERVICE and PERFORMANCE.
+            </p>
+          </div>
+          <div className="w-full lg:w-1/2">
+            <img
+              className="lg:inline hidden w-3/4"
+              src={aboutimage}
+              alt="people discussing on board"
+            />
+            <img
+              className="lg:hidden sm:block hidden w-full"
+              src={aboutimage}
+              alt="people discussing on board"
+            />
+            <img
+              className="sm:hidden block w-full"
+              src={aboutimage}
+              alt="people discussing on board"
+            />
+          </div>
+        </div>
+
+        <div className="flex lg:flex-row flex-col md:gap-14 gap-16 justify-between lg:mt-20 mt-16">
+          <div className="w-full lg:w-6/12 h-full text-center">
+            <h2 className="font-bold lg:text-4xl text-3xl lg:leading-9 text-center leading-7 text-gray-800 h-1/3">
+              Our Mision & Values
+            </h2>
+            <div className="w-full md:h-2/3">
+              <img src={image} alt="logo" className="lg:inline hidden w-3/4" />
+              <img
+                className="lg:hidden sm:block hidden w-full"
+                src={image}
+                alt="people discussing on board"
+              />
+              <img
+                className="sm:hidden block w-full"
+                src={image}
+                alt="people discussing on board"
+              />
+            </div>
+            <p className="text-center">
+              To be the FIRST and CONSISTENT choice of Patients, Physicians,
+              Purchasers & Investors in practice of wound healing management.
+            </p>
+          </div>
+          <div className="w-full lg:w-6/12">
+            <h1 className="font-semibold text-3xl md:text-5xl">
+              Why <span className="text-green-400">Lifekshell</span>
+            </h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 lg:gap-8 gap-10">
+              <div className="flex p-2 shadow-md">
+                <div className="">
+                  <p className="font-semibold lg:text-2xl text-xl lg: leading-5 text-gray-800">
+                    Efficient
+                  </p>
+                  <p className=" font-normal text-base  text-gray-600">
+                    Quick and safe service with high punctuality and accuracy.
+                  </p>
+                </div>
+              </div>
+              <div className="flex p-2 shadow-md">
+                <div className="">
+                  <p className="font-semibold lg:text-2xl text-xl lg: leading-5 text-gray-800">
+                    Compassionate
+                  </p>
+                  <p className=" font-normal text-base  text-gray-600">
+                    NPWT dressing service by well-trained, dedicated and caring
+                    team of dressing executives
+                  </p>
+                </div>
+              </div>
+              <div className="flex p-2 shadow-md">
+                <div className="">
+                  <p className="font-semibold lg:text-2xl text-xl lg: leading-5 text-gray-800">
+                    Supportive
+                  </p>
+                  <p className=" font-normal text-base  text-gray-600">
+                    Guidance and counseling to patients and families about
+                    dressing procedures, wound care and hygiene
+                  </p>
+                </div>
+              </div>
+              <div className="flex p-2 shadow-md">
+                <div className="">
+                  <p className="font-semibold lg:text-2xl text-xl lg: leading-5 text-gray-800">
+                    Progressive
+                  </p>
+                  <p className=" font-normal text-base  text-gray-600">
+                    An open and transparent feedback system aimed at constant
+                    improvement.
+                  </p>
+                </div>
+              </div>
+
+              <div className="flex p-4 shadow-md">
+                <div className="">
+                  <p className="font-semibold lg:text-2xl text-xl lg: leading-5 text-gray-800">
+                    Accessible
+                  </p>
+                  <p className=" font-normal text-base  text-gray-600">
+                    On-call service facility and provide services at hospital &
+                    at home.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <h1 className="font-normal max-md:text-4xl text-center">
+        Reviews of Literature :{" "}
+        <span className="font-semibold"> NPWT in Orthopaedic Surgery</span>
+      </h1>
+      <Reviews />
+    </Layout>
+  )
+}
+
+export default about
